de656e5997f633496a3eaf72e7f79acefa60ac77
Jordan A. Holt <jordanh316@gmail.com>
2023-05-14 23:50:33 -0500

    Compiled all files. Next, build for deployment

--- Store.jsx
+++
@@ -1,9 +0,0 @@
-import {configureStore} from '@reduxjs/toolkit';
-
-import editorReducer from './editorSlice'
- 
-export default configureStore({
-    reducer:{
-        editor: editorReducer}
-})
-
